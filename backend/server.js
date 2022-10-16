7095d4d97fd0c7900c685d937d2f5e6a715f71cb
laminemessaci <lamine.messaci18@gmail.com>
2022-10-16 20:36:28 +0200

    Create models and connect to mongoose our server

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,19 @@
 import express from 'express';
-import products from './data/products.js';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import colors from 'colors'
+import colors from 'colors';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 
 const app = express();
 connectDB();
+
 app.get('/', (req, res) => {
   res.send('Api is ranning ...');
 });
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
-
+app.use('/api/products', productRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(
