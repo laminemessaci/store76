e05a8524500d09a449b8c40f4fbf190587ae2a8d
laminemessaci <lamine.messaci18@gmail.com>
2022-10-17 09:51:12 +0200

    Add custom erro middlware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -12,8 +13,8 @@ connectDB();
 app.get('/', (req, res) => {
   res.send('Api is ranning ...');
 });
-
 app.use('/api/products', productRoutes);
+app.use(notFound, errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(
