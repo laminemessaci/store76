4c44198def81ac6358eb49f838afecf4eb84d892
laminemessaci <lamine.messaci18@gmail.com>
2022-10-22 23:24:42 +0200

    Configure User Authentication endpoint

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,22 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 dotenv.config();
+connectDB();
 
 const app = express();
-connectDB();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Api is ranning ...');
 });
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+
 app.use(notFound, errorHandler);
 const PORT = process.env.PORT || 5000;
 
