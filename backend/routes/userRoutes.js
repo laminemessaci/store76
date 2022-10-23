e15dda439f5a81d6d9a6cfc0f348f112b6ad88be
laminemessaci <lamine.messaci18@gmail.com>
2022-10-23 18:59:10 +0200

    Configure login & register User routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,15 @@
 import express from 'express';
-import { authUser } from '../controllers/userController.js';
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+} from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddlwire.js';
 
 const router = express.Router();
 
+router.post('/', registerUser);
 router.post('/login', authUser);
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;
