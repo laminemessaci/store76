ad8cf7dcb03140f6e0567cfd4ab425f824eb65b8
lamminemessaci <lamine.messaci18@gmail.com>
2023-10-24 12:12:30 +0200

    Add cors

---
+++ backend/config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+  "localhost:5000",
+  "http://localhost:3000",
+  "http://127.0.0.1:5000",
+];
+
+export default allowedOrigins;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
 
-import Footer from './components/Footer.js';
-import Header from './components/Header.js';
-import CartScreen from './screens/CartScreen.js';
-import HomeScreen from './screens/HomeScreen.js';
-import LoginScreen from './screens/LoginScreen.js';
-import OrderScreen from './screens/OrderScreen.js';
-import PlaceOrderScreen from './screens/PlaceOrderScreen.js';
-import ProductEditScreen from './screens/ProductEditScreen.js';
-import ProductListScreen from './screens/ProductListScreen.js';
-import ProductScreen from './screens/ProductScreen.js';
-import ProfileScreen from './screens/ProfileScreen.js';
-import PaymentScreen from './screens/PaymentScreen.js';
-import RegisterScreen from './screens/RegisterScreen.js';
-import ShippingScreen from './screens/ShippingScreen.js';
-import UserEditScreen from './screens/UserEditScreen.js';
-import UserListScreen from './screens/UserListScreen.js';
-import OrderListScreen from './screens/OrderListScreen.js';
+import Footer from "./components/Footer.js";
+import Header from "./components/Header.js";
+import CartScreen from "./screens/CartScreen.js";
+import HomeScreen from "./screens/HomeScreen.js";
+import LoginScreen from "./screens/LoginScreen.js";
+import OrderScreen from "./screens/OrderScreen.js";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen.js";
+import ProductEditScreen from "./screens/ProductEditScreen.js";
+import ProductListScreen from "./screens/ProductListScreen.js";
+import ProductScreen from "./screens/ProductScreen.js";
+import ProfileScreen from "./screens/ProfileScreen.js";
+import PaymentScreen from "./screens/PaymentScreen.js";
+import RegisterScreen from "./screens/RegisterScreen.js";
+import ShippingScreen from "./screens/ShippingScreen.js";
+import UserEditScreen from "./screens/UserEditScreen.js";
+import UserListScreen from "./screens/UserListScreen.js";
+import OrderListScreen from "./screens/OrderListScreen.js";
+import axios from "axios";
 
 const App = () => {
+  axios.defaults.baseURL = "https://store76-api.onrender.com/";
+  axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+  axios.defaults.headers.common["Access-Control-Allow-Methods"] =
+    "GET,PUT,POST,DELETE,PATCH,OPTIONS";
   return (
     <>
       <Header />
